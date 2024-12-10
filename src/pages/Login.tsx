6d6b476e90bd036d9611cd821f44e0198a0f32c4
Andrew Yupin <brolo1341@gmail.com>
2024-12-10 19:21:17 +0200

    filled the pages with blocks and initial data

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,55 @@
 import { Link } from "react-router-dom";
+import { TextInput } from "../shared/components/input/input";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [errorEmailMessage, setErrorEmailMessage] = useState<string>("");
+
+  const [name, setName] = useState("");
+  const [errorNameMessage, setErrorNameMessage] = useState<string>("");
+
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const handleChange = (e: any) => {
+  };
+
+  const onSubmit = async (e: any) => {
+  };
   return (
     <div className="page-container">
       <div className="form-container">
         <h2>Login Page</h2>
         <p>Welcome to the login page</p>
+
+        <TextInput
+          type="text"
+          value={name}
+          onChange={handleChange}
+          id="name"
+          label="Name"
+          required
+          error={errorNameMessage}
+        />
+
+        <TextInput
+          type="text"
+          value={email}
+          onChange={handleChange}
+          id="email"
+          label="Email"
+          required
+          error={errorEmailMessage}
+        />
+
+        <button
+          type="submit"
+          name="action"
+          onClick={onSubmit}
+          disabled={!email && !name}
+        >
+          {loading ? `loading...` : `save`}
+        </button>
 
         <Link to="/reset-password">Go to Reset Pass</Link>
         <Link to="/registration">Go to Registration</Link>
--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { TextInput } from '../shared/components/input/input';
 
 const ResetPassword = () => {
+  const [email, setEmail] = useState('');
+  const [errorEmailMessage, setErrorEmailMessage] = useState('');
+
+
+  const handleChange = (e: any) => {
+  };
+
+
   return (
     <div className="page-container">
       <div className="form-container">
         <h2>Reset Password Page</h2>
         <p>Welcome to the Reset Password page</p>
+        <TextInput
+          type="text"
+          value={email}
+          onChange={handleChange}
+          id="email"
+          label="Email"
+          required
+          error={errorEmailMessage}
+        />
         <Link to="/login">Go to Login</Link>
       </div>
     </div>
